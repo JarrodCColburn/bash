3295163ac411255f0a5d5c84bfb8a712aa561741
JarrodCColburn <jcourtlandcolburn@gmail.com>
2019-01-22 11:49:54 -0600

    install vscode

--- startup.sh
+++ startup.sh
@@ -17,3 +17,11 @@ antigen bundle zsh-users/zsh-syntax-highlighting
 antigen theme robbyrussell
 # Tell Antigen that you're done.
 antigen apply" >> $HOME/.zshrc
+
+# install vsCode, per https://code.visualstudio.com/docs/setup/linux#_installation
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+yes | sudo apt-get install apt-transport-https
+yes | sudo apt-get update
+yes | sudo apt-get install code
